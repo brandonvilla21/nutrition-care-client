f96f0687b62d68d52c447f2650c972f41406fe70
Brandon Villa <bornofos@gmail.com>
2018-03-09 17:54:47 -0600

    Component that used to handle the form, now just call the Component that handles the form

--- src/containers/admin/Food/CreateFoodPage.js
+++ src/containers/admin/Food/CreateFoodPage.js
@@ -1,119 +1,52 @@
 import React, { Component } from 'react';
-import PageBase from '../../../components/PageBase';
-import { TextField } from 'material-ui';
-import RaisedButton from 'material-ui/RaisedButton/RaisedButton';
-import axios from 'axios';
-import urlConfig from '../../../url-config';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import FoodForm from '../../../components/Food/FoodForm';
 
 class CreateFoodPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            description: '',
-            carbohydrates: '',
-            proteins: '',
-            fats: '',
-            calories: ''
+            submitted: false
         };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.isSubmitted = this.isSubmitted.bind(this);
+        this.handleClose = this.handleClose.bind(this);
     }
 
-    handleChange( event ) {
-        const name = event.target.name;
-        const value = event.target.value;
-
-        this.setState({
-            [name]: value
-        });
+    isSubmitted( submitted ) {
+        this.setState({ submitted });
     }
-
-    handleSubmit( event ) {
-        event.preventDefault();
-        const url = `${urlConfig.baseUrl}/foods`;
-        const config = {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'}
-        }
-        axios.post(url, this.state, config)
-            .then( response => {
-                if (response.status === 200) {
-                    
-                }
-            })
-            .catch(err => console.log('ERROR:  ' + err));
-
+    
+    handleClose() {
+        this.setState({ submitted: false });
     }
 
     render() {
-        return(
-            <PageBase
-                title="Registrar un alimento"
-                navigation="Alimentos / Registro">
-                
-                <form id="food-form" onSubmit={this.handleSubmit}>
-                <TextField
-                    hintText="Descripción"
-                    floatingLabelText="Descripción"
-                    value={this.state.description}
-                    name="description"
-                    onChange={this.handleChange}
-                    fullWidth={true} />
-
-                <TextField
-                    hintText="Carbohidratos"
-                    type="number"
-                    floatingLabelText="Carbohidratos"
-                    value={this.state.carbohydrates}
-                    name="carbohydrates"
-                    onChange={this.handleChange}
-                    fullWidth={true} />
-
-                <TextField
-                    hintText="Proteínas"
-                    floatingLabelText="Proteínas"
-                    type="number"
-                    value={this.state.proteins}
-                    name="proteins"
-                    onChange={this.handleChange}                    
-                    fullWidth={true} />
-
-                <TextField
-                    hintText="Grasas"
-                    floatingLabelText="Grasas"
-                    type="number"
-                    value={this.state.fats}
-                    name="fats"
-                    onChange={this.handleChange}                    
-                    fullWidth={true} />
-
-                <TextField
-                    hintText="Calorías"
-                    floatingLabelText="Calorías"
-                    type="number"
-                    value={this.state.calories}
-                    name="calories"
-                    onChange={this.handleChange}                    
-                    fullWidth={true} />
-
-                <RaisedButton
-                    label="Registrar alimento"
-                    primary={true}
-                    type="submit"
-                    style={styles.button} />
-                </form>
-
-            </PageBase>
+        const actions = [
+            <FlatButton
+                key={0}
+                label="Aceptar"
+                primary={true}
+                onClick={this.handleClose}
+            />
+        ];
+        return (
+            <div>
+                <Dialog
+                    title="Alimento registrado"
+                    actions={actions}
+                    modal={false}
+                    open={this.state.submitted}
+                    onRequestClose={this.handleClose}
+                >
+                    El alimento ha sido registrado con éxito.
+                </Dialog>
+                <FoodForm
+                    onSubmitted={this.isSubmitted}
+                />
+            </div>
         );
     }
 }
 
 export default CreateFoodPage;
-
-const styles = {
-    button: {
-        margin: 10,
-        float: 'right'
-    }
-};
