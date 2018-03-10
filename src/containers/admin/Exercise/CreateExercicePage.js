7fbf1262b4f28f725afaf100000ef044e7fe644c
Brandon Villa <bornofos@gmail.com>
2018-03-09 19:14:56 -0600

    CreateExercicePage is stable now

--- src/containers/admin/Exercise/CreateExercicePage.js
+++ src/containers/admin/Exercise/CreateExercicePage.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
 import ExerciceForm from '../../../components/Exercice/ExerciceForm';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 
 class CreateExercicePage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+          submitted: false
+        };
+
+        this.handleClose = this.handleClose.bind(this);
+        this.isSubmitted = this.isSubmitted.bind(this);
+    }
+    
+    handleClose() {
+      this.setState({ submitted: false });
+    }
+
+    isSubmitted( submitted ) {
+      this.setState({ submitted });
     }
 
     render() {
+      const actions = [
+          <FlatButton
+              key={0}
+              label="Aceptar"
+              primary={true}
+              onClick={this.handleClose}
+          />
+      ];
       return (
         <div>
-          <ExerciceForm />
+            <Dialog
+              title="Ejercicio registrado"
+              actions={actions}
+              modal={false}
+              open={this.state.submitted}
+              onRequestClose={this.handleClose}
+            >
+              El ejercicio ha sido registrado con éxito.
+            </Dialog>
+          <ExerciceForm onSubmitted={this.isSubmitted} />
         </div>
       );
     }
