8fc551188173ae8033c6d3959d1a12d6ae8bc566
Brandon Villa <bornofos@gmail.com>
2018-03-10 10:13:20 -0600

    ExercicePage now fetch the exercices from the server and display them

--- src/containers/admin/Exercise/ExercisePage.js
+++ src/containers/admin/Exercise/ExercisePage.js
@@ -1,12 +1,49 @@
 import React, { Component } from 'react';
-import { green600 } from 'material-ui/styles/colors';
+import { green600, grey500 } from 'material-ui/styles/colors';
 import { Link } from 'react-router';
 import PageBase from '../../../components/PageBase';
-import { Table, TableRow, FloatingActionButton, TableHeader } from 'material-ui';
+import { Table, TableRow, TableRowColumn, FloatingActionButton, TableHeader, TableBody } from 'material-ui';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 import TableHeaderColumn from 'material-ui/Table/TableHeaderColumn';
+import urlConfig from '../../../url-config';
+import Image from 'material-ui-image';
 
 class ExercisePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            exercices: []
+        };
+    }
+
+    componentWillMount() {
+        this.getExercices()
+            .then(exercices => this.setState({ exercices }));
+    }
+
+    getExercices() {
+        const url = `${urlConfig.baseUrl}/exercises`;
+        return fetch(url)
+            .then( data => data.json())
+            .then( response => response.data);
+    }
+
+    renderRows() {
+        return this.state.exercices.map( exercice => 
+            <TableRow key={exercice.id}>
+                <TableRowColumn style={styles.columns.image} >
+                    <Image 
+                        src={`${urlConfig.imageDir}/${exercice.srcImage}`}
+                        color="white"
+                        style={styles.image}
+                    />
+                </TableRowColumn>
+                <TableRowColumn style={{...styles.columns.name, ...styles.biggerFont}} >{exercice.name}</TableRowColumn>
+                <TableRowColumn style={{...styles.columns.date, ...styles.biggerFont}} >{exercice.created_at.date.substring(0,11)}</TableRowColumn>
+            </TableRow>
+        );
+    }
+
     render() {
         return(
             <PageBase
@@ -24,13 +61,14 @@ class ExercisePage extends Component {
                         displaySelectAll={false}
                         adjustForCheckbox={false}>
                         <TableRow>
-                            <TableHeaderColumn>Imagen</TableHeaderColumn>
-                            <TableHeaderColumn>Descripción</TableHeaderColumn>
+                            <TableHeaderColumn style={styles.columns.image}>Imagen</TableHeaderColumn>
+                            <TableHeaderColumn style={styles.columns.name}>Nombre</TableHeaderColumn>
+                            <TableHeaderColumn style={styles.columns.date}>Fecha de creación</TableHeaderColumn>
                         </TableRow>
-                        {/* <TableBody>
-
-                        </TableBody> */}
                     </TableHeader>
+                    <TableBody displayRowCheckbox={false}>
+                        {this.renderRows()}
+                    </TableBody>
                 </Table>
                 </div>
                 
@@ -46,6 +84,31 @@ const styles = {
         bottom: 20,
         left: 'auto',
         position: 'fixed',
-    }
+    },
+    image: {
+        width: 'auto'
+    },
+    biggerFont: {
+        fontSize: '1.3em'
+    },
+    columns: {
+        image: {
+            width: '30%'
+        },
+        name: {
+            width: '35%',
+            textAlign: 'center'
+        },
+        edit: {
+
+        },
+        date: {
+            width: '35%',
+            textAlign: 'center'
+        }
+    },
+    editButton: {
+        fill: grey500
+    },
 };
 export default ExercisePage;
