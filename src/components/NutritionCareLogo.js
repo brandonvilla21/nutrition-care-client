ebb23be3959e59173faa10b7650f22dee86927ab
Brandon Villa <bornofos@gmail.com>
2018-03-12 16:47:50 -0600

    An attempt to create Nutriotion Care logo

---
+++ src/components/NutritionCareLogo.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const NutritionCareLogo = () => {
+    const styles = {
+        container: {
+            // display: 'flex',
+            // justifyContent: 'flex-start',
+            // alignItems: 'flex-end',
+            // marginTop: 0,
+
+            position: 'absolute',
+            top: '200px',
+            left: '-30px',
+            zIndex: '1000',
+            height: '50px !important',
+            overflow: 'hidden',
+            height: '200px',
+            // margin: 'auto'
+            // backgroundImage:  'url(' + require('../images/nutrition_care.png') + ')',
+            // zIndex: '100',
+            // height: '200px',
+            // width: '200px'
+        },
+        img: {
+            height: '280px',
+            backgroundColor: 'white',
+            // boxShadow: '5px 10px #888888'
+        }
+    }
+    return(
+        <div style={styles.container}>
+            <img style={styles.img} src="../images/nutrition_care.png" />
+        </div>
+    );
+};
+
+export default NutritionCareLogo;
