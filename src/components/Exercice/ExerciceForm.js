e237c64a46a2fdd8a078ce2a2e48d50c238068ff
marcos8896 <elyomarcos@gmail.com>
2018-03-14 20:11:19 -0600

    Added React Table to the ExerciceForm component.

--- src/components/Exercice/ExerciceForm.js
+++ src/components/Exercice/ExerciceForm.js
@@ -6,6 +6,13 @@ import FileBase64 from 'react-file-base64';
 import { typography } from 'material-ui/styles';
 import axios from 'axios';
 import urlConfig from '../../url-config';
+import ReactTable from 'react-table';
+import Divider from 'material-ui/Divider';
+import 'react-table/react-table.css';
+import Moment from 'moment';
+import { Tabs, Tab } from "material-ui/";
+import { blue500 } from 'material-ui/styles/colors';
+// var moment = require('moment');
 
 class ExerciceForm extends Component {
     constructor(props) {
@@ -28,6 +35,21 @@ class ExerciceForm extends Component {
         this.setState({
             [name]: value
         });
+    }
+
+    componentWillMount() {
+      this.getBodyAreas()
+          .then(bodyAreas => {
+            console.log(bodyAreas);
+            this.setState({ bodyAreas })
+          });
+    }
+
+    getBodyAreas() {
+        const url = `${urlConfig.baseUrl}/bodyareas`;
+        return fetch(url)
+            .then( data => data.json())
+            .then( response => response.data);
     }
 
     handleSubmit( event ) {
@@ -53,7 +75,8 @@ class ExerciceForm extends Component {
         
     disableButton() {
         return  this.state.name === '' 
-                || this.state.base64_image === '';
+                || this.state.base64_image === ''
+                || this.state.bodyAreas === [];
     }
     
     clearState() {
@@ -64,6 +87,8 @@ class ExerciceForm extends Component {
     }
 
     render() {
+        const { bodyAreas } = this.state;
+
         return(
             <PageBase
                 title="Registrar un ejercicio"
@@ -78,17 +103,65 @@ class ExerciceForm extends Component {
                     onChange={this.handleChange}
                     fullWidth={true} />
 
-                    <h3 style={styles.imageTitle}>Agrega una imagen</h3>
-                    <FileBase64 
-                        multiple={false}
-                        onDone={this.getFiles}
-                    />
+
+                <Tabs style={styles.tabs}>
+
+                  <Tab style={styles.tab} label="Seleccionar datos generales" >
+                    <div>
+
+                      <br/>
+
+                      <ReactTable
+                        filterable
+                        style={{width: '100%'}}
+                        data={bodyAreas}
+                        columns={[
+                          {
+                            Header: "ID",
+                            accessor: "id",
+                            maxWidth: 100
+                          },
+                          {
+                            Header: "Descripción",
+                            accessor: "description"
+                          }
+                        ]}
+                        defaultPageSize={10}
+                        className="-striped -highlight"
+                      />
+
+                    </div>
+                  </Tab>
+                  <Tab style={styles.tab} label="Seleccionar imagen" >
+                    <div>
+                      
+                      <h3 style={styles.imageTitle}>Agrega una imagen</h3>
+
+                      <FileBase64 
+                          multiple={false}
+                          onDone={this.getFiles}
+                      />
+                      
+                      <div style={styles.imageDisplay}>
+                        <img src={this.state.base64_image}/>
+                      </div>
+
+
+                    </div>
+                  </Tab>
+                </Tabs>
+                        
+                    {/* </div> */}
+
+                {/* <Divider/> */}
+
                 <RaisedButton
                     label="Registrar ejercicio"
                     primary={true}
                     type="submit"
                     disabled={this.disableButton()}
                     style={styles.button} />
+
                 </form>
 
             </PageBase>
@@ -109,7 +182,25 @@ const styles = {
         fontWeight: typography.fontWeightLight,
         marginBottom: 20,
         marginTop: 15
+    },
+    imageDisplay: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 'auto 0'  
+    },
+    tab: {
+      backgroundColor: blue500,
+    },
+    tabs: {
+      paddingTop: '18px'
     }
+    // imageStyle: {
+    //   width: ''
+    // },
+    // rowContainer: {
+    //   display: 'flex'
+    // }
 
 };
   
