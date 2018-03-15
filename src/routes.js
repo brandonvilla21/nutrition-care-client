4d64d7c8fff7a5d613981de69ee956bed99fb096
Brandon Villa <bornofos@gmail.com>
2018-03-15 10:26:58 -0600

    login route is handled by LoginPage component

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
 import NotFoundPage from './containers/NotFoundPage.js';
-import LoginPage from './containers/LoginPage';
+import LoginPage from './containers/session/LoginPage';
 import FormPage from './containers/FormPage';
 import TablePage from './containers/TablePage';
 import Dashboard from './containers/DashboardPage';
