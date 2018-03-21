c34267fc620f6d0b919e829a1f769f9977d8b0f0
Brandon Villa <bornofos@gmail.com>
2018-03-20 19:13:44 -0600

    Component in charge of handling the second tab in TabsRutine component

---
+++ src/components/Rutine/MyRutine.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SelectDay from '../../components/Rutine/SelectDay';
+
+class MyRutine extends Component {
+    render() {
+      return (
+        <div>
+          <SelectDay />
+        </div>
+      );
+    }
+}
+
+export default MyRutine;
