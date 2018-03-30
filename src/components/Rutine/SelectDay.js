1d297878d36dabae42baa2ed2846407906683291
Brandon Villa <bornofos@gmail.com>
2018-03-30 00:14:23 -0600

    Now updates selected days dynamically

--- src/components/Rutine/SelectDay.js
+++ src/components/Rutine/SelectDay.js
@@ -5,36 +5,72 @@ class SelectDay extends Component {
     constructor() {
         super();
         this.state = {
-            value: 0
+            value: 0,
+            days: [...days]
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.menuItems = this.menuItems.bind(this);
+        this.updateSelectDays = this.updateSelectDays.bind(this);
     }
 
-    handleChange(event, index, value) {
-        this.setState({ value }, () => this.props.addDay(event, index, value));
+    componentDidUpdate() {
+        const { removedDay } = this.props;
+        if ( removedDay ) {
+            this.updateSelectDays();
+        }
     }
 
     menuItems() {
-      return days.map( day => (
-        <MenuItem
-          key={day}
-          insetChildren={true}
-          value={day}
-          primaryText={day}
-        />
+      return this.state.days.map( day => (
+        !day.selected
+            ? <MenuItem
+                key={day.id}
+                insetChildren={true}
+                value={day}
+                primaryText={day.name}
+                />
+            : null
       ));
     }
 
+    handleChange(event, index, value) {
+        this.setState({ value }, () => {
+            const index = this.state.days.indexOf(value);
+            days[index].selected = true;
+            this.props.selectedDays(this.state.days.filter( day => day.selected));
+        });
+    }
+
+    updateSelectDays() {
+        const { removedDay } = this.props;
+        const { days } = this.state;
+
+        // Get the new day to add to <SelectField />
+        const addDayToSelect = days.filter( day => day.id === removedDay.id );
+        // Get the days that are currently in <SelectField />
+        const restOfDays = days.filter(day => day.id !== removedDay.id );
+        
+        // Change the selected property to false, because now this day has not been selected
+        addDayToSelect[0].selected = false;
+        
+        // Merge both elements in one array
+        const sortedDays = [...restOfDays, addDayToSelect[0]];
+        // Sorts the elements by its id
+        sortedDays.sort((a,b) => a.id - b.id);
+        
+        // Update the state with the new days
+        this.setState({ days: [...sortedDays]});
+        
+        this.props.clearRemovedDay();
+    }
     render() {
-        const { value } = this.state;
         return (
             <div>
                 <SelectField
                     floatingLabelText="Seleccione un día"
-                    value={value}
                     onChange={this.handleChange}
+                    style={styles.selectField}
                 >
                     {this.menuItems()}
                 </SelectField>
@@ -44,17 +80,26 @@ class SelectDay extends Component {
 }
 
 const days = [
-    "Lunes",
-    "Martes",
-    "Miércoles",
-    "Jueves",
-    "Viernes",
-    "Sábado",
-    "Domingo",
+    { id: 0, name: 'Lunes', selected: false },
+    { id: 1, name: 'Martes', selected: false },
+    { id: 2, name: 'Miércoles', selected: false },
+    { id: 3, name: 'Jueves', selected: false },
+    { id: 4, name: 'Viernes', selected: false },
+    { id: 5, name: 'Sábado', selected: false },
+    { id: 6, name: 'Domingo', selected: false },
 ];
 
+const styles = {
+    selectField: {
+        width: '100%'
+    }
+};
+
 SelectDay.propTypes = {
-    addDay: PropTypes.func
+    days: PropTypes.array,
+    selectedDays: PropTypes.func,
+    removedDay: PropTypes.object,
+    clearRemovedDay: PropTypes.func
 };
 
 export default SelectDay;
