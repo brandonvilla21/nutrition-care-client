f4130ef0bb8bc8854762acedecec1a54ebb9fe1d
marcos8896 <elyomarcos@gmail.com>
2018-04-09 18:35:25 -0500

    #3 - Added first structure for register-diets feature.

---
+++ src/containers/user/Diet/CreateDietPage.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+
+class CreateDietPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          submitted: false
+        };
+
+        this.handleClose = this.handleClose.bind(this);
+        this.isSubmitted = this.isSubmitted.bind(this);
+    }
+    
+    handleClose() {
+      this.setState({ submitted: false });
+    }
+
+    isSubmitted( submitted ) {
+      this.setState({ submitted });
+    }
+
+    render() {
+      const actions = [
+          <FlatButton
+              key={0}
+              label="Aceptar"
+              primary={true}
+              onClick={this.handleClose}
+          />
+      ];
+      return (
+        <div>
+            <Dialog
+              title="Dieta registrada"
+              actions={actions}
+              modal={false}
+              open={this.state.submitted}
+              onRequestClose={this.handleClose}
+            >
+              El ejercicio ha sido registrado con éxito.
+            </Dialog>
+          {/* <ExerciceForm onSubmitted={this.isSubmitted} /> */}
+          <p>It worked</p>
+        </div>
+      );
+    }
+}
+
+export default CreateDietPage;
