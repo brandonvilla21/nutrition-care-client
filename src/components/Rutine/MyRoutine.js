af233903eadd4b51d506278a46d81f4503c9fb6f
Brandon Villa <bornofos@gmail.com>
2018-04-11 10:08:11 -0500

    Next and Prev buttons added to second tab

--- src/components/Rutine/MyRoutine.js
+++ src/components/Rutine/MyRoutine.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import SelectDay from '../../components/Rutine/SelectDay';
 import RoutineDay from './RoutineDay';
+import { RaisedButton } from 'material-ui';
 
 class MyRoutine extends Component {
   constructor(props) {
@@ -24,10 +25,32 @@ class MyRoutine extends Component {
                 removeDay={this.props.removeDay}
                 day={day}
                 onChangeField={this.props.onChangeField} />)}
+          
+          <RaisedButton
+                    label="Regresar"
+                    secondary={true}
+                    style={styles.raisedButtonPrev}
+                    value={0}
+                    onClick={this.props.prevIndex} />
+          <RaisedButton
+                    label="Siguiente"
+                    primary={true}
+                    style={styles.raisedButtonNext}
+                    value={2}
+                    onClick={this.props.nextIndex} />
         </div>
       );
   }
 }
+
+const styles = {
+  raisedButtonPrev: {
+    float: 'left'
+  },
+  raisedButtonNext: {
+    float: 'right'
+  },
+};
 
 MyRoutine.propTypes = {
   selectedDays: PropTypes.func,
@@ -37,6 +60,8 @@ MyRoutine.propTypes = {
   clearRemovedDay: PropTypes.func,
   addExerciseToDay: PropTypes.func,
   onChangeField: PropTypes.func,
+  nextIndex: PropTypes.func,
+  prevIndex: PropTypes.func,
 };
 
 export default MyRoutine;
