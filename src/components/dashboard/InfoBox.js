ff0836cfb210c7113925f32697fa376f5b428690
Brandon Villa <bornofos@gmail.com>
2018-04-15 20:12:05 -0500

    action props added to component

--- src/components/dashboard/InfoBox.js
+++ src/components/dashboard/InfoBox.js
@@ -1,4 +1,4 @@
-import React, {PropTypes} from 'react';
+import React, { PropTypes } from 'react';
 import Paper from 'material-ui/Paper';
 import {white, grey800} from 'material-ui/styles/colors';
 import {typography} from 'material-ui/styles';
@@ -6,13 +6,18 @@ import {typography} from 'material-ui/styles';
 class InfoBox extends React.Component {
 
   render() {
-    const {color, title, value, Icon} = this.props;
+    const {color, title, value, Icon, action} = this.props;
 
     const styles = {
       content: {
-        padding: '5px 10px',
-        marginLeft: 90,
-        height: 80
+        padding: '5px 20px 5px 30px',
+        marginLeft: 110,
+        height: 120
+      },
+      action: {
+        display: 'block',
+        margin: '30px 0',
+        textAlign: 'right'
       },
       number: {
         display: 'block',
@@ -27,31 +32,35 @@ class InfoBox extends React.Component {
       },
       iconSpan: {
         float: 'left',
-        height: 90,
-        width: 90,
+        height: 130,
+        width: 130,
         textAlign: 'center',
         backgroundColor: color
       },
       icon: {
-        height: 48,
-        width: 48,
-        marginTop: 20,
+        height: 68,
+        width: 68,
+        marginTop: 35,
         maxWidth: '100%'
-
       }
     };
 
     return (
       <Paper>
-        <span style={styles.iconSpan}>
-          <Icon color={white}
-                style={styles.icon}
-          />
-        </span>
+        <div>
+          <span style={styles.iconSpan}>
+            <Icon color={white}
+                  style={styles.icon}
+            />
+          </span>
 
-        <div style={styles.content}>
-          <span style={styles.text}>{title}</span>
-          <span style={styles.number}>{value}</span>
+          <div style={styles.content}>
+            <span style={styles.text}>{title}</span>
+            <span style={styles.number}>{value}</span>
+            <div style={styles.action}>
+              {action}
+            </div>
+          </div>
         </div>
       </Paper>
       );
@@ -62,7 +71,8 @@ InfoBox.propTypes = {
   Icon: PropTypes.any, // eslint-disable-line
   color: PropTypes.string,
   title: PropTypes.string,
-  value: PropTypes.string
+  value: PropTypes.string,
+  action: PropTypes.func
 };
 
 export default InfoBox;
