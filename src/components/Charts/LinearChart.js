b7a5cb3cffd40e41db88c5f6d6910fe94a3727ec
Brandon Villa <bornofos@gmail.com>
2018-04-18 15:28:58 -0500

    Unnecesary imports removed

--- src/components/Charts/LinearChart.js
+++ src/components/Charts/LinearChart.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { ResponsiveContainer, AreaChart, Area, LineChart, Line, Tooltip, YAxis, XAxis, CartesianGrid } from 'recharts';
+import { LineChart, Line, Tooltip, YAxis, XAxis, CartesianGrid } from 'recharts';
 import { blue400 } from 'material-ui/styles/colors';
 import ReactResizeDetector from 'react-resize-detector';
 
@@ -33,7 +33,7 @@ class LinearChart extends Component {
                 max = Math.max( ...dataKeyArray );
         
         return (
-            <div ref={ e => {this.chartContainer = e}}>
+            <div ref={e => {this.chartContainer = e;}}>
                 <ReactResizeDetector handleWidth handleHeight onResize={this.onResize} />
 
                 <LineChart width={this.state.width} height={300} data={this.props.data} margin={{ top: 5, right: 0, bottom: 0, left: 0 }}>
@@ -48,7 +48,7 @@ class LinearChart extends Component {
             </div>
         );
     }
-};
+}
 
 LinearChart.propTypes = {
     data: PropTypes.array,
