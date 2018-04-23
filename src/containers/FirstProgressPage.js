8dc487db36f28985291e96bbb153ab7ddc6a9aec
Brandon Villa <bornofos@gmail.com>
2018-04-23 16:28:50 -0500

    Dialog added to component

--- src/containers/FirstProgressPage.js
+++ src/containers/FirstProgressPage.js
@@ -2,17 +2,43 @@ import React, { Component, PropTypes } from 'react';
 import { MuiThemeProvider } from 'material-ui/styles';
 import ThemeDefault from '../theme-default';
 import CreateProgressPage from './user/UserInfo/CreateProgressPage';
+import { Dialog, FlatButton } from 'material-ui';
 
 class FirstProgressPage extends Component {
     constructor() {
         super();
-        
+        this.state = {
+            open: true,
+        };
+
+        this.onClose = this.onClose.bind(this);
+    }
+    
+    onClose() {
+        this.setState({ open: false });
     }
 
     render() {
+        const actions = [
+            <FlatButton
+              key={0}
+              label="Aceptar"
+              primary={true}
+              onClick={this.onClose}
+            />
+        ];
         return (
             <MuiThemeProvider muiTheme={ThemeDefault}>
                 <div style={styles.container}>
+                <Dialog
+                    title="Bienvenido a Nutrition Care"
+                    actions={actions}
+                    modal={true}
+                    open={this.state.open}
+                    onRequestClose={this.onClose}
+                >
+                    Bienvenido a Nutrition Care. Registra tu primer progreso para iniciar en la plataforma.
+                </Dialog>
                     <CreateProgressPage />
                 </div>
             </MuiThemeProvider>
